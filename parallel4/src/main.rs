11afae9613e1c5e66a9aa4c8e7a4ebe74050bdfd
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-09-08 14:00:47 +0000

    バリア同期

---
+++ parallel4/src/main.rs
@@ -0,0 +1,19 @@
+use std::sync::{Arc, Barrier};
+use std::thread;
+fn main() {
+    let mut v = Vec::new();
+    let barrier = Arc::new(Barrier::new(10));
+
+    for i in 0..10 {
+        let b = barrier.clone();
+        let th = thread::spawn(move || {
+            b.wait();
+            println!("finished barrier: {}", i);
+        });
+        v.push(th);
+    }
+
+    for th in v {
+        th.join().unwrap();
+    }
+}
