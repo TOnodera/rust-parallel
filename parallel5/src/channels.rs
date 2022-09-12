cdc671c4b44abb4945f7c70a55018d28925b1142
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-09-12 13:38:07 +0000

    プロデューサー/コンシューマモデル実装

---
+++ parallel5/src/channels.rs
@@ -0,0 +1,53 @@
+use crate::semaphore::Semaphore;
+use std::collections::LinkedList;
+use std::sync::{Arc, Condvar, Mutex};
+
+#[derive(Clone)]
+pub struct Sender<T> {
+    sem: Arc<Semaphore>,
+    buf: Arc<Mutex<LinkedList<T>>>,
+    cond: Arc<Condvar>,
+}
+
+impl<T: Send> Sender<T> {
+    pub fn send(&self, data: T) {
+        self.sem.wait();
+        let mut buf = self.buf.lock().unwrap();
+        buf.push_back(data);
+        self.cond.notify_one();
+    }
+}
+
+pub struct Reciver<T> {
+    sem: Arc<Semaphore>,
+    buf: Arc<Mutex<LinkedList<T>>>,
+    cond: Arc<Condvar>,
+}
+
+impl<T> Reciver<T> {
+    pub fn recv(&self) -> T {
+        let mut buf = self.buf.lock().unwrap();
+        loop {
+            if let Some(data) = buf.pop_front() {
+                self.sem.post();
+                return data;
+            }
+            buf = self.cond.wait(buf).unwrap();
+        }
+    }
+}
+
+pub fn channel<T>(max: isize) -> (Sender<T>, Reciver<T>) {
+    assert!(max > 0);
+    let sem = Arc::new(Semaphore::new(max));
+    let buf = Arc::new(Mutex::new(LinkedList::new()));
+    let cond = Arc::new(Condvar::new());
+    let tx = Sender {
+        sem: sem.clone(),
+        buf: buf.clone(),
+        cond: cond.clone(),
+    };
+
+    let rx = Reciver { sem, buf, cond };
+    (tx, rx)
+}
--- parallel5/src/main.rs
+++ parallel5/src/main.rs
@@ -1,3 +1,5 @@
+/*
+mod channels;
 mod semaphore;
 use semaphore::Semaphore;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -28,6 +30,43 @@ fn main() {
                 unsafe { CNT.fetch_sub(1, Ordering::SeqCst) };
 
                 s.post();
+            }
+        });
+        v.push(t);
+    }
+
+    for t in v {
+        t.join().unwrap();
+    }
+}
+*/
+
+mod channels;
+mod semaphore;
+
+use channels::channel;
+
+const NUM_LOOP: usize = 100000;
+const NUM_THREADS: usize = 8;
+
+fn main() {
+    let (tx, rx) = channel(4);
+    let mut v = Vec::new();
+    let t = std::thread::spawn(move || {
+        let mut cnt = 0;
+        while cnt < NUM_THREADS * NUM_LOOP {
+            let n = rx.recv();
+            println!("recv: n = {:?}", n);
+            cnt += 1;
+        }
+    });
+    v.push(t);
+
+    for i in 0..NUM_THREADS {
+        let tx0 = tx.clone();
+        let t = std::thread::spawn(move || {
+            for j in 0..NUM_LOOP {
+                tx0.send((i, j));
             }
         });
         v.push(t);
