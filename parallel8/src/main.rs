a34721c8a01e616c03d052425ab3ce6d5bb4a004
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-09-15 15:21:26 +0000

    疑似覚醒について

---
+++ parallel8/src/main.rs
@@ -0,0 +1,15 @@
+use signal_hook::{consts::SIGUSR1, iterator::Signals};
+
+use std::{error::Error, process, thread, time::Duration};
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("pid: {}", process::id());
+    let mut signals = Signals::new(&[SIGUSR1])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("received signal: {:?}", sig);
+        }
+    });
+
+    thread::sleep(Duration::from_secs(10));
+    Ok(())
+}
