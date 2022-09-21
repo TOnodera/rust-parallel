907a20fa8e9a9f9c71d8c0a8182fad93359d2677
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-09-21 15:40:24 +0000

    04まで

--- parallel14/src/main.rs
+++ parallel14/src/main.rs
@@ -6,17 +6,36 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
-        println!("1");
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
-    let mut connection = Connection::new(socket);
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
 
-        let response = Frame::Error("unimplemented".to_string());
+    let mut db = HashMap::new();
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
         connection.write_frame(&response).await.unwrap();
     }
 }
