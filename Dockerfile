4386af829adf3b31814b53e3952c2e19c95fd259
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-10-17 12:22:03 +0000

    コミット

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,13 @@ FROM rust:latest
 RUN useradd -m -u 1000 -s /bin/bash rust 
 RUN rustup component add rustfmt
 RUN apt update -y && apt install telnet
+
+# chrome
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add && \
+    echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list && \
+    apt-get update && \
+    apt-get install -y google-chrome-stable && \
+    apt-get -y clean && \
+    rm -rf /var/lib/apt/lists/*
+
 USER rust
--- parallel18/src/main.rs
+++ parallel18/src/main.rs
@@ -1,38 +1,69 @@
-use futures::task;
+use crossbeam::channel;
+use futures::task::{self, ArcWake};
 use std::collections::VecDeque;
 use std::future::Future;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
+use std::thread;
 use std::time::{Duration, Instant};
-
 struct MiniTokio {
-    tasks: VecDeque<Task>,
+    scheduled: channel::Receiver<Arc<Task>>,
+    sender: channel::Sender<Arc<Task>>,
 }
 
-type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>,
+}
 
+impl Task {
+    fn poll(self: Arc<Self>) {
+        let waker = task::waker(self.clone());
+        let mut cx = Context::from_waker(&waker);
+
+        let mut future = self.future.try_lock().unwrap();
+        let _ = future.as_mut().poll(&mut cx);
+    }
+
+    fn spawn<F>(future: F, sender: &channel::Sender<Arc<Task>>)
+    where
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: sender.clone(),
+        });
+
+        let _ = sender.send(task);
+    }
+
+    fn schedule(self: &Arc<Self>) {
+        self.executor.send(self.clone());
+    }
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        arc_self.schedule();
+    }
+}
 impl MiniTokio {
     fn new() -> Self {
-        Self {
-            tasks: VecDeque::new(),
-        }
+        let (sender, scheduled) = channel::unbounded();
+        Self { scheduled, sender }
     }
 
     fn spawn<F>(&mut self, future: F)
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        self.tasks.push_back(Box::pin(future));
+        Task::spawn(future, &self.sender);
     }
 
     fn run(&mut self) {
-        let waker = task::noop_waker();
-        let mut cx = Context::from_waker(&waker);
-
-        while let Some(mut task) = self.tasks.pop_front() {
-            if task.as_mut().poll(&mut cx).is_pending() {
-                self.tasks.push_back(task);
-            }
+        while let Ok(task) = self.scheduled.recv() {
+            task.poll();
         }
     }
 }
@@ -48,7 +79,18 @@ impl Future for Delay {
             println!("Hello, World");
             Poll::Ready("done")
         } else {
-            cx.waker().wake_by_ref();
+            // 現在のタスクに紐づく"waker"ハンドルを取得
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            // タイマースレッドをspawn
+            thread::spawn(move || {
+                let now = Instant::now();
+                if now < when {
+                    thread::sleep(when - now);
+                    waker.wake();
+                }
+            });
             Poll::Pending
         }
     }
